9a30a9c7b032ca251a834e9fdc1901253c04478b
camric <ymontantlointain@gmail.com>
2022-04-12 12:57:39 +0200

    login page

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-img src="../assets/icon.png" style="width: 80%" />
+    <LoginForm />
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import LoginForm from "components/LoginForm.vue";
+
+export default defineComponent({
+  name: "LoginPage",
+
+  components: {
+    LoginForm,
+  },
+});
+</script>
